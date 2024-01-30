987476775136bec9212d7557be43be06134b4eb0
skamuju08 <skamuju@ucsd.edu>
2024-01-30 14:54:33 -0800

    Update

---
+++ Lab2/ChatServer.java
@@ -0,0 +1,30 @@
+package Lab2;
+
+import java.io.IOException;
+import java.net.URI;
+
+class Handler implements URLHandler {
+    // The one bit of state on the server: a number that will be manipulated by
+    // various requests.
+    int num = 0;
+
+    public String handleRequest(URI url) {
+        if (url.getPath().equals("/")) {
+            return String.format("Number: %d", num);
+        } else {
+            return "Error";
+        }
+    }
+}
+
+class ChatServer {
+    public static void main(String[] args) throws IOException {
+        if(args.length == 0){
+            System.out.println("Missing port number! Try any number between 1024 to 49151");
+            return;
+        }
+
+        int port = Integer.parseInt(args[0]);
+        Server.start(port, new Handler());
+    }
+}
